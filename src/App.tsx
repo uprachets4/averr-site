f2c36030a7c766e41d8e2ef33bdb794bb100b47f
Prachets Upadhyay <uprachets4@gmail.com>
2026-07-24 22:47:27 -0400

    Initial commit: Nav, Hero (Three.js scene), ProofOfWork section with SIFT slider and logos

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Nav from './components/Nav';
+import Hero from './components/Hero';
+import ProofOfWork from './components/ProofOfWork';
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <Nav />
+      <Hero />
+      <ProofOfWork />
+    </div>
+  );
+}
+
+export default App;
